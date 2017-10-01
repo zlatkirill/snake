0e65b3a524a20a5b0bdf5eb17c70b7aaf75ad557
zlat <zlat91@mail.ri>
2017-10-01 18:16:19 +0300

    02

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -9,7 +9,24 @@ namespace snake
     class Program
     {
         static void Main(string[] args)
+            
+
         {
+            int x1 = 1;
+            int y1 = 3;
+            char symbol = '*';
+
+            Draw(x1, y1, symbol);
+            int x2 = 4;
+            int y2 = 5;
+            char symbol2 = '*';
+            Draw(x2, y2, symbol2);
+            Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
