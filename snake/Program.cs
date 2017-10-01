6495a8220d3183be80095550aa26147830cfd8f9
zlat <zlat91@mail.ri>
2017-10-01 19:17:14 +0300

    1917

---
+++ snake/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '#';
+            p1.Draw();
+
+
+            Point p2 = new Point();
+            p2.x = 7;
+            p2.y = 3;
+            p2.sym = '$';
+            p2.Draw();
+            Console.ReadLine();
+        }
+
+
+    }
+}
